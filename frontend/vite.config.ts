18bdbbfa68befebf212050d5df72d657e4c6b261
Llocya Saravia <llocyasaraviajuan@gmail.com>
2025-07-04 13:21:47 -0500

    frontend actualziado

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   server: {
     proxy: {
-      // Redirige todas las peticiones que empiecen con /api al backend Flask
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
