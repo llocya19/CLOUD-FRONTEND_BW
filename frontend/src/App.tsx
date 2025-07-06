3a3587467340a1c759cd624786b7259ca8b081cd
Llocya Saravia <llocyasaraviajuan@gmail.com>
2025-07-06 11:55:12 -0500

    FRONTEND actualziado en despliegue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,6 +21,7 @@ import { Sidebar } from './components/ui/Sidebar';
 import Toast from './components/ui/Toast';
 
 import { AuthProvider, useAuth } from './context/AuthContext';
+import useInactividad from './hooks/useInactividad';
 
 function AppWrapper() {
   return (
@@ -39,7 +40,10 @@ function App() {
     type: 'success',
   });
 
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
+
+  // ⏱️ Cerrar sesión automáticamente tras 2 minutos (120000 ms) de inactividad
+  useInactividad(logout, 2 * 60 * 1000);
 
   const handleToggleMenu = () => {
     setShowSidebar(!showSidebar);
